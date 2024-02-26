f70b4682cc952ae7b9b38df2c8d30f4c207887ef
MiguelPasaje <cartel-dy@hotmail.com>
2024-02-26 13:44:51 -0500

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { AfiliadosModule } from './afiliados/afiliados.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AfiliadosModule,
+    SharedModule,
     HttpClientModule,
     ReactiveFormsModule
   ],
